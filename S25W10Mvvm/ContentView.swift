755b86787a1ba9b1f3a7049115620ae400b2fabd
rhkralsgh <knh239@naver.com>
2025-11-10 16:44:52 +0900

    💩@State 사용

--- S25W10Mvvm/ContentView.swift
+++ S25W10Mvvm/ContentView.swift
@@ -1,17 +1,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(count)")
+            
+            HStack {
+                Button("감소") {
+                    count -= 1
+                }
+                
+                Button("증가") {
+                    count += 1
+                }
+            }
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
